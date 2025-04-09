1566f8ec31ca042d0a5e4c27ce1ecaa8c67224eb
erick21340 <Torot0086@gmail.com>
2025-04-09 01:33:30 -0900

    Creamos el EntrenadoresScreen

--- app/src/main/java/com/adielgarcia/gympro/presentation/entrenadores/EntrenadoresScreen.kt
+++ app/src/main/java/com/adielgarcia/gympro/presentation/entrenadores/EntrenadoresScreen.kt
@@ -1,4 +1,4 @@
-package com.adielgarcia.gympro.presentation.entrenadores
+package  com.adielgarcia.gympro.presentation.entrenadores
 
 import android.util.Log
 import androidx.compose.foundation.clickable
