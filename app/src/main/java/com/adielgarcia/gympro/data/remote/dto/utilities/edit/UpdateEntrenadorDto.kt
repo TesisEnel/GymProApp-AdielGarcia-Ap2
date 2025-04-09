f4588cf9a0ad2d2301b89bb87c755eea85a03e92
erick21340 <Torot0086@gmail.com>
2025-04-09 01:42:41 -0900

    Creamos  el UpdateEntrenadorDto

--- app/src/main/java/com/adielgarcia/gympro/data/remote/dto/utilities/edit/UpdateEntrenadorDto.kt
+++ app/src/main/java/com/adielgarcia/gympro/data/remote/dto/utilities/edit/UpdateEntrenadorDto.kt
@@ -1,4 +1,4 @@
-package com.adielgarcia.gympro.data.remote.dto.utilities.edit
+package  com.adielgarcia.gympro.data.remote.dto.utilities.edit
 
 data class UpdateEntrenadorDto(
     val entrenadorId: Int,
