e5d887e6843a2f0c83387c258bc158944490fe8b
erick21340 <Torot0086@gmail.com>
2025-04-09 01:39:12 -0900

    Creamos  el ProductoDialog

--- app/src/main/java/com/adielgarcia/gympro/presentation/productos/ProductoDialog.kt
+++ app/src/main/java/com/adielgarcia/gympro/presentation/productos/ProductoDialog.kt
@@ -1,4 +1,4 @@
-package com.adielgarcia.gympro.presentation.productos
+package  com.adielgarcia.gympro.presentation.productos
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
