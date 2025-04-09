29f88130f1a6328f99bee93f32f6d7d5e1dc87e5
erick21340 <Torot0086@gmail.com>
2025-04-09 01:19:22 -0900

    Creamos el Resource

--- app/src/main/java/com/adielgarcia/gympro/data/remote/Resource.kt
+++ app/src/main/java/com/adielgarcia/gympro/data/remote/Resource.kt
@@ -1,4 +1,4 @@
-package com.adielgarcia.gympro.data.remote
+package  com.adielgarcia.gympro.data.remote
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
     class Success<T>(data: T): Resource<T>(data)
